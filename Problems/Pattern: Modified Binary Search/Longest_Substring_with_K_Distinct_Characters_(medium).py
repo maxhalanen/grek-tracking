a54eae99dcbb84e0765ab891c2d180c82d424f34
maxhalanen <71463707+maxhalanen@users.noreply.github.com>
2024-10-21 00:19:43 -0400

    Create Longest_Substring_with_K_Distinct_Characters_(medium).py

---
+++ Problems/Pattern: Modified Binary Search/Longest_Substring_with_K_Distinct_Characters_(medium).py
@@ -0,0 +1,31 @@
+class Solution:
+  def findLength(self, str1, k):
+
+      chars = {}
+      start = 0
+      count = 0
+      maxCount = 0
+      arr = list(str1)
+      
+      for i in range(len(str1)):
+
+        if arr[i] in chars:
+          chars[arr[i]] += 1
+        else:
+          chars[arr[i]] = 1
+        
+        count += 1
+
+        if len(chars) > k:
+          while len(chars) > k:
+
+            chars[arr[start]] -= 1
+            count -= 1
+            if chars[arr[start]] == 0:
+              chars.pop(arr[start])    
+            start += 1
+        print(count)
+        maxCount = max(count, maxCount)          
+
+      # TODO: Write your code here
+      return maxCount
